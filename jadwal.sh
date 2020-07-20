607aa4a9702d24b6d41095f6045242b9a3f04ff4
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2020-07-20 15:10:48 +0700

    Update jadwal.sh

--- jadwal.sh
+++ jadwal.sh
@@ -23,7 +23,7 @@ read -p "Nama-Kota-Kamu : " kota
 echo
 echo $cy"[...Mengumpulkan Data²...]"$pu
 sleep 3
-curl https://jadwalsholat.pkpu.or.id$kota
+curl https://jadwalsholat.pkpu.or.id/$kota
 echo
 read -p "MAU BALIK LAGI {Y/T} : " pil
 if [ $pil = Y ] || [ $pil = y ]
